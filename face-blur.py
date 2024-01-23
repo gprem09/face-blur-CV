8206ff40c74c259fecc294b1fa33776367019988
gpa21 <gpa21@sfu.ca>
2024-01-22 20:54:08 -0800

    Used Haar Cascade XML file for face detection

--- face-blur.py
+++ face-blur.py
@@ -1,1 +1,19 @@
-print('hello world')
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow('Webcam', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
