7cf79590a6b8d300488b18d36efe0e3a44fb4e9b
gpa21 <gpa21@sfu.ca>
2024-01-22 20:56:56 -0800

    improved blur accuracy

--- face-blur.py
+++ face-blur.py
@@ -8,11 +8,11 @@ while True:
     if not ret:
         break
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
-        face = frame[y:y + h, x:x + w]
+        face = frame[y:y+h, x:x+w]
         face = cv2.GaussianBlur(face, (99, 99), 30)
-        frame[y:y + face.shape[0], x:x + face.shape[1]] = face
+        frame[y:y+face.shape[0], x:x+face.shape[1]] = face
     cv2.imshow('Webcam', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
